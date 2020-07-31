cc1a32c68876ef4afe711f6d2fb64ee8df3a465e
Juanjo Garcia Ripoll <juanjose.garciaripoll@gmail.com>
2020-07-31 12:33:14 +0200

    Fixed make.cmd, which did not delete data/ and seemp/ directories

--- make.cmd
+++ make.cmd
@@ -3,9 +3,8 @@ if "%1" == "all" (
     python -c "import exportnb; import glob; exportnb.export_notebooks(glob.glob('*.ipynb'),verbose=True); quit()"
 )
 if "%1" == "clean" (
-    rmdir /S /Q seeq
+    rmdir /S /Q seemps data
     del /Q core_mps.py core.py mpi*.py job*.py
-    del /Q  make.cmd
 )
 if "%1" == "cleanup" (
     for %%i in (*.ipynb); do jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace "%%i"
